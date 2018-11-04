b93ce21104d79eda654b6033f74714958f84e916
devtoni <devtonirm@gmail.com>
2018-11-04 18:51:19 +0100

    refactor(tictactoe): enum players

--- src/main/java/com/katas/tictactoe/TicTacToe.java
+++ src/main/java/com/katas/tictactoe/TicTacToe.java
@@ -2,17 +2,35 @@ package com.katas.tictactoe;
 
 public class TicTacToe {
 
-    private String player;
+    private Players player;
 
     public TicTacToe(){
-        player = "X";
+        player = Players.X;
     }
 
     public String currentPlayer() {
-        return player;
+        return player.representation;
     }
 
     public void nextTurn() {
-        player = player.equals("X") ? "O" : "X";
+        player = Players.of(player).equals("X") ? Players.O : Players.X;
+    }
+
+    private enum Players {
+        X("X"), O("O");
+
+        private final String representation;
+
+        Players(String representation) {
+            this.representation = representation;
+        }
+
+        public static String of(Players player) {
+            return player.representation;
+        }
     }
 }
+
+
+// nextTurn (Row 2: Col 2)
+   // board.place(currnt, 2, 2)
--- src/test/java/com/katas/tictactoe/TicTacToeShould.java
+++ src/test/java/com/katas/tictactoe/TicTacToeShould.java
@@ -27,4 +27,5 @@ public class TicTacToeShould {
 
         assertThat(ticTacToe.currentPlayer(), is("X"));
     }
+
 }
